cedf8bb14f28422a23f42e44a950f3a94ffad8d2
Andy Friesen <andy.friesen@gmail.com>
2022-05-19 19:24:31 -0700

    Rewrite everything.  Better.

--- src/voorhees.rs
+++ src/voorhees.rs
@@ -1,5 +1,4 @@
-
-use std::str::CharIndices;
+use std::str::from_utf8;
 
 #[derive(Debug, PartialEq)]
 pub enum Value {
@@ -12,129 +11,176 @@ pub enum Value {
 #[derive(Debug, PartialEq)]
 pub struct ParseError(String);
 
-fn is_whitespace(c: char) -> bool {
-    c == ' ' || c == '\t' || c == '\n' || c == '\r'
+struct Lexer<'a> {
+    s: &'a [u8],
+    pos: usize
 }
 
-fn is_identifier(c: char) -> bool {
-    (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-}
+impl<'a> Lexer<'a> {
+    fn new(s: &[u8]) -> Lexer {
+        Lexer{s: s, pos: 0}
+    }
 
-fn peek(chars: &CharIndices) -> Option<(usize, char)> {
-    let mut clone = chars.clone();
-    clone.next()
-}
+    fn eof(&self) -> bool {
+        self.pos >= self.s.len()
+    }
 
-fn eat_whitespace(chars: &mut CharIndices) -> Option<usize> {
-    while let Some((ofs, ch)) = peek(chars) {
-        if is_whitespace(ch) {
-            chars.next();
-        } else {
-            return Some(ofs);
+    fn advance(&mut self) {
+        if !self.eof() {
+            self.pos += 1;
         }
     }
 
-    return None;
-}
-
-fn slice(s: &str, start: usize, end: Option<usize>) -> (&str, &str, &str) {
-    let (first, second) = s.split_at(start);
-    if let Some(e) = end {
-        let (third, fourth) = second.split_at(e - start);
-        (first, third, fourth)
-    } else {
-        (first, second, "")
-    }
-}
-
-fn lex(s: &str) -> (&str, &str) {
-    if 0 == s.len() {
-        return (s, s);
-    }
-
-    let mut chars = s.char_indices();
-    let start_offset = match eat_whitespace(&mut chars) {
-        Some(o) => o,
-        None => return ("", "")
-    };
-
-    let (char_offset, c) = chars.next().unwrap();
-
-    if let Some(_) = "{}[]:,".find(c) {
-        if let Some((end_offset, c)) = chars.next() {
-            let (f, ch, rest) = slice(s, start_offset, Some(end_offset));
-            println!("symbol '{}' rest='{}'", ch, rest);
-            (ch, rest)
+    fn peek_byte(&self) -> Option<u8> {
+        if self.eof() {
+            None
         } else {
-            (s, "")
+            Some(self.s[self.pos])
         }
-        
-    } else if is_identifier(c) {
-        while let Some((ofs, ch)) = chars.next() {
-            if !is_identifier(ch) {
-                let (a, b, c) = slice(s, start_offset, Some(ofs));
-                return (b, c);
+    }
+
+    fn take_byte(&mut self) -> Option<u8> {
+        if self.eof() {
+            None
+        } else {
+            let res = self.s[self.pos];
+            self.advance();
+            Some(res)
+        }
+    }
+
+    fn take<T>(&mut self, pred: T) -> Option<u8> where T : FnOnce(u8) -> bool {
+        if let Some(ch) = self.peek_byte() {
+            if pred(ch) {
+                return Some(ch)
             }
         }
-        (s, "")
-    } else {
-        (s, s)
+        return None;
+    }
+
+    fn take_while<T>(&mut self, pred: T) -> &'a [u8] where T : Fn(u8) -> bool {
+        let start_pos = self.pos;
+        while let Some(ch) = self.peek_byte() {
+            if pred(ch) {
+                self.advance();
+            } else {
+                break;
+            }
+        }
+
+        &self.s[start_pos..self.pos]
+    }
+
+    fn skip_whitespace(&mut self) {
+        self.take_while(|ch| ch == ' ' as u8 || ch == '\t' as u8  || ch == '\r' as u8  || ch == '\n' as u8);
+    }
+
+    fn is_identifier_start(b: u8) -> bool {
+        (b >= 'a' as u8 && b <= 'z' as u8) || (b >= 'A' as u8 && b <= 'Z' as u8) || b == '_' as u8
+    }
+
+    fn is_identifier_char(b: u8) -> bool {
+        Self::is_identifier_start(b) || (b >= '0' as u8 && b <= '9' as u8)
+    }
+
+    fn token(&mut self) -> &'a [u8] {
+        self.skip_whitespace();
+
+        if self.eof() {
+            return &[];
+        }
+
+        let next_char = |lexer: &mut Self| {
+            let start_pos = lexer.pos;
+            lexer.advance();
+            &lexer.s[start_pos..lexer.pos]
+        };
+
+        let byte = self.peek_byte().unwrap();
+
+        let result = match byte as char {
+            '[' => next_char(self),
+            ']' => next_char(self),
+            ',' => next_char(self),
+            ':' => next_char(self),
+            '{' => next_char(self),
+            '}' => next_char(self),
+            _ if Self::is_identifier_start(byte) => {
+                self.take_while(Self::is_identifier_char)
+            },
+            _ => {
+                next_char(self)
+            }
+        };
+
+        self.skip_whitespace();
+        
+        result
+    }
+
+    fn rest(&self) -> &'a [u8] {
+        &self.s[self.pos..self.s.len()]
     }
 }
 
 pub fn parse(s: &str) -> Result<Value, ParseError> {
-    let (v, rest) = parse_(s)?;
+    let mut lexer = Lexer::new(s.as_bytes());
+    let v = parse_(&mut lexer)?;
 
-    if rest.len() > 0 {
+    lexer.skip_whitespace();
+
+    if !lexer.eof() {
         Err(ParseError(
-            "Extra goop at the end of the file: ".to_owned() + rest,
+            "Extra goop at the end of the file: ".to_owned() + from_utf8(lexer.rest()).unwrap(),
         ))
     } else {
         Ok(v)
     }
 }
 
-pub fn parse_(s: &str) -> Result<(Value, &str), ParseError> {
-    let (next, mut rest) = lex(s);
-    if next.len() == 0 {
+const NULL_TOKEN: &'static [u8] = b"null";
+const TRUE_TOKEN: &'static [u8] = b"true";
+const FALSE_TOKEN: &'static [u8] = b"false";
+const OPEN_BRACKET_TOKEN: &'static [u8] = b"[";
+const CLOSE_BRACKET_TOKEN: &'static [u8] = b"]";
+const OPEN_BRACE_TOKEN: &'static [u8] = b"{";
+const CLOSE_BRACE_TOKEN: &'static [u8] = b"}";
+const COLON_TOKEN: &'static [u8] = b":";
+const COMMA_TOKEN: &'static [u8] = b",";
+
+fn parse_(lexer: &mut Lexer) -> Result<Value, ParseError> {
+    let token = lexer.token();
+    println!("token '{}'", from_utf8(token).unwrap());
+
+    if token.len() == 0 {
         Err(ParseError("Unexpected end of document".to_owned()))
-    } else if next == "null" {
-        Ok((Value::Null, rest))
-    } else if next == "true" {
-        Ok((Value::Boolean(true), rest))
-    } else if next == "false" {
-        Ok((Value::Boolean(false), rest))
-    } else if next == "[" {
+    } else if token == NULL_TOKEN {
+        Ok(Value::Null)
+    } else if token == TRUE_TOKEN {
+        Ok(Value::Boolean(true))
+    } else if token == FALSE_TOKEN {
+        Ok(Value::Boolean(false))
+    } else if token == OPEN_BRACKET_TOKEN {
         let mut arr = Vec::new();
         loop {
-            let (value, rest_) = parse_(rest)?;
-            rest = rest_;
+            let val = parse_(lexer)?;
+            arr.push(val);
 
-            arr.push(value);
-
-            if rest_.len() == 0 {
-                return Err(ParseError("Unexpected end of document".to_owned()));
-            }
-
-            let (next, rest_) = lex(rest);
-            rest = rest_;
-
-            if next == "]" {
+            let next = lexer.token();
+            if next == CLOSE_BRACKET_TOKEN{
                 break;
-            } else if next == "," {
+            } else if next == COMMA_TOKEN {
                 continue;
-            } else if next == "" {
+            } else if next.len() == 0 {
                 return Err(ParseError("Unexpected end of document".to_owned()));
             } else {
-                return Err(ParseError(
-                    "Expected ',' or ']' but got '".to_owned() + next + "'",
-                ));
+                return Err(ParseError("Expected ',' or ']' but got '".to_owned() + from_utf8(next).unwrap() + "'"));
             }
         }
 
-        Ok((Value::Array(arr), rest))
+        Ok(Value::Array(arr))
     } else {
-        Err(ParseError("Unknown token '".to_owned() + next + "'"))
+        Err(ParseError("Unknown token '".to_owned() + from_utf8(token).unwrap() + "'"))
     }
 }
 
@@ -155,9 +201,7 @@ fn simple_array() {
 fn nested_array() {
     let expected = Value::Array(vec![
         Value::Boolean(true),
-        Value::Array(
-            vec![Value::Boolean(false), Value::Null]
-        )
+        Value::Array(vec![Value::Boolean(false), Value::Null]),
     ]);
 
     assert_eq!(Ok(expected), parse("[true,[false,null]]"));
@@ -165,10 +209,7 @@ fn nested_array() {
 
 #[test]
 fn whitespace() {
-    let expected = Value::Array(vec![
-        Value::Boolean(true),
-        Value::Boolean(false)
-    ]);
+    let expected = Value::Array(vec![Value::Boolean(true), Value::Boolean(false)]);
 
     assert_eq!(Ok(expected), parse(" [ true , false ] "));
 }
